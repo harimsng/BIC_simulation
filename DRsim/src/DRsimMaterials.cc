e682f059f6a363a794c6c80325bf26c32171fe09
seo.yun.jang@cern.ch <seo.yun.jang@cern.ch>
2024-06-04 13:04:39 +0900

    Changed PMT

--- DRsim/src/DRsimMaterials.cc
+++ DRsim/src/DRsimMaterials.cc
@@ -171,9 +171,9 @@ void DRsimMaterials::CreateMaterials() {
 
   G4double refl_SiPM[nEnt]; std::fill_n(refl_SiPM, nEnt, 0.);
   G4double eff_SiPM[nEnt] = {
-    0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.10,
-    0.11, 0.13, 0.15, 0.17, 0.19, 0.20, 0.22, 0.23,
-    0.24, 0.25, 0.24, 0.23, 0.21, 0.20, 0.17, 0.14, 0.10
+    0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00,
+    0.00, 0.00, 0.00, 0.00, 0.01, 0.02, 0.03, 0.07,
+    0.11, 0.13, 0.17, 0.19, 0.21, 0.21, 0.21, 0.20, 0.19
   };
   mpSiPM = new G4MaterialPropertiesTable();
   mpSiPM->AddProperty("REFLECTIVITY",opEn,refl_SiPM,nEnt);
